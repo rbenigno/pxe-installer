45f829609efbad73d173267e30700db5ba271cde
Ryan Benigno <ryanb@pobox.com>
2015-02-18 09:55:34 -0800

    Configure PXE host as an internet gateway

---
+++ README.md
@@ -0,0 +1,9 @@
+# PXE Provisioning Server
+
+## Purpose
+
+## How to use 
+
+## License
+
+This content is licensed under the MIT License.
--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-SRC=/vagrant/pxe-srv
+SRC=/vagrant/pxe-installer
 BOOTFILES=/provisioning
-HTTP_HOST=10.199.199.10
+INSTALL_HOST=10.199.199.10
 
 ## Install required packages
 sudo apt-get update -q
@@ -16,6 +16,14 @@ service nginx reload
 ## Configure dnsmasq
 cp $SRC/dnsmasq.conf /etc
 
+## Configure NAT
+mv /etc/ufw/before.rules /etc/ufw/before.rules.original
+cp $SRC/before.rules /etc/ufw/
+sed -e "s/DEFAULT_INPUT_POLICY=\"DROP\"/DEFAULT_INPUT_POLICY=\"ACCEPT\"/g" -i /etc/default/ufw
+sed -e "s/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g" -i /etc/default/ufw
+sed -e "s/#net\/ipv4\/ip_forward=1/net\/ipv4\/ip_forward=1/g" -i /etc/ufw/sysctl.conf
+ufw disable && sudo ufw --force enable
+
 ## Create provisioning folder
 mkdir $BOOTFILES
 cd $BOOTFILES
@@ -26,14 +34,14 @@ cp /usr/lib/syslinux/menu.c32 $BOOTFILES
 
 ## Drop in our PXE configuration file
 mkdir $BOOTFILES/pxelinux.cfg
-sed -e "s/<HTTP_HOST>/$HTTP_HOST/g" $SRC/pxelinux.cfg/default > $BOOTFILES/pxelinux.cfg/default
+sed -e "s/<HTTP_HOST>/$INSTALL_HOST/g" $SRC/pxelinux.cfg/default > $BOOTFILES/pxelinux.cfg/default
 
 ## Extract ISO for ESXi 5.5u2 (Cisco)
 7z x -o"$BOOTFILES/esxi_5.5u2_cisco" $SRC/Vmware-ESXi-5.5.0-2068190-custom-Cisco-5.5.2.2.iso
 ## Files must be lower case
 rename 'y/A-Z/a-z/' $BOOTFILES/esxi_5.5u2_cisco/*.*
 ## Edit the boot.cfg to use HTTP
-sed -e "s/\//http:\/\/$HTTP_HOST\/esxi_5.5u2_cisco\//g" -i $BOOTFILES/esxi_5.5u2_cisco/boot.cfg
+sed -e "s/\//http:\/\/$INSTALL_HOST\/esxi_5.5u2_cisco\//g" -i $BOOTFILES/esxi_5.5u2_cisco/boot.cfg
 
 ## Extract ISO for Ubuntu 14.04 Server
 7z x -o"$BOOTFILES/ubuntu-14.04-srv" $SRC/ubuntu-14.04.1-server-amd64.iso
